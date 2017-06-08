6db230f88ff5192529be0bb0fe207964bd2dd18e
swetha siva <swethadav@gmail.com>
2017-06-08 22:05:31 +0530

    Add files via upload
    trying with score updates

---
+++ gametry2.js
@@ -0,0 +1,244 @@
+    var canvas= document.querySelector('canvas'); // getting the canvas element into canvas variable
+    canvas.width=window.innerWidth;// resizing canvas height to full width
+    canvas.height=window.innerHeight; // resizing canvas height to full height
+   
+
+
+
+var c= canvas.getContext('2d');
+var score =0 ;;
+var cl=5;
+var game=1;
+var msg=0;
+
+          
+           var x1=100;
+           var y1=300;
+
+            function upArrowPressed()
+           {  if( y1-50>0)
+                y1-=5;
+               draw();
+            }
+
+            function downArrowPressed()
+           { if(y1+50<innerHeight)
+                y1+=5;
+               draw();
+            }
+
+            function moveSelection(event) {                    
+                switch (event.keyCode) {
+
+                    case 38:
+                        upArrowPressed();
+                    break;
+
+                    case 40:
+                        downArrowPressed();
+                    break;
+                }
+            };
+
+        function gameLoop()
+        {
+            
+            moveSelection();
+            setTimeout("gameLoop()",5000);
+        }
+
+        
+
+function draw ()
+{
+   //c.clearRect(0,0,innerWidth,innerHeight);
+    
+c.beginPath();
+c.arc(x1,y1,50,1.5*Math.PI,0.5*Math.PI);
+c.lineWidth=5;
+c.strokeStyle="black";
+c.stroke();
+    
+c.beginPath();
+c.moveTo(x1,y1-50);
+c.lineTo(x1,y1+50);
+c.lineWidth=2;
+c.stroke();  
+
+    c.beginPath();
+    c.moveTo(x1,y1);
+    c.lineTo(x1+75,y1);
+    c.lineWidth=2;
+    c.stroke();
+    
+    c.beginPath();
+    c.moveTo(x1+65,y1+10);
+    c.lineTo(x1+75,y1);
+    c.lineWidth=2;
+    c.stroke();
+    
+    c.beginPath();
+    c.moveTo(x1+65,y1-10);
+    c.lineTo(x1+75,y1);
+    c.lineWidth=2;
+    c.stroke();
+    
+    
+    c.font = "30px Arial";
+c.strokeText("SCORE:",10,50);
+c.strokeText(score,150,50);
+if(cl>=0)
+    {
+c.font = "30px Arial";
+c.strokeText("ARROWS LEFT :",10,80);
+c.strokeText(cl,300,80); }
+    else 
+        {
+c.font = "30px Arial";
+c.strokeText("GAME OVER !!! RESET TO PLAY AGAIN",300,300);
+ }
+   
+}
+
+
+ var y= Math.random()*innerHeight;
+var dy = 1;
+
+
+
+
+function animate()
+{
+    requestAnimationFrame(animate);
+     c.clearRect(0,0,innerWidth,innerHeight);
+    draw();
+  
+    
+c.beginPath();
+c.arc(1100,y,10,0,Math.PI*2,false);
+c.lineWidth= 10;
+c.strokeStyle="red";
+c.stroke();
+
+c.beginPath();
+c.arc(1100,y,30,0,Math.PI*2,false);
+c.lineWidth= 10;
+c.strokeStyle="yellow";
+c.stroke();
+
+c.beginPath();
+c.arc(1100,y,50,0,Math.PI*2,false);
+c.lineWidth= 10;
+c.strokeStyle="green";
+c.stroke();
+    
+    
+     
+if(y+50 > innerHeight || y - 50 < 0)
+    {
+        dy= -1 * dy;
+    }
+
+   if(game==1)
+   {y+=dy;}
+    
+}
+ var x2=100;
+
+
+function firearrow()
+{ if(game==1){
+    cl--;
+    if(cl>=0)
+        {
+            shoot();
+        }
+    else
+    {   
+        draw1();
+    }
+}
+}
+
+function shoot()
+{ 
+     if(x2<1100 )
+    {  requestAnimationFrame(shoot);
+    c.beginPath();
+    c.moveTo(x2,y1);
+    c.lineTo(x2+75,y1);
+    c.lineWidth=2;
+    c.stroke(); 
+     
+     c.beginPath();
+    c.moveTo(x2+65,y1+10);
+    c.lineTo(x2+75,y1);
+    c.lineWidth=2;
+    c.stroke();
+    
+    c.beginPath();
+    c.moveTo(x2+65,y1-10);
+    c.lineTo(x2+75,y1);
+    c.lineWidth=2;
+    c.stroke();
+     
+    x2+=5; 
+     
+     
+     
+  }
+     else
+{    if(y+20>y1 && y-20<y1)
+    {  score += 100;}
+ else if(y+100>y1 && y-100<y1 && y+20<y1 && y-20>y1)
+    {  score += 50;}
+ else if(y+300>y1 && y-300<y1 && y+50<y1 && y-50>y1 && y+20<y1 && y-20>y1)
+    {  score += 25;}
+ 
+     x2=100;
+ 
+}
+   
+} 
+ 
+
+
+function reset()
+{
+    cl=5;
+    score=0;
+}
+
+function stop()
+{
+    game=0;
+}
+
+function start()
+{
+    game=1;
+}
+
+/*function display()
+{game=0;
+    
+ msg=1;
+ disp();
+    
+   
+     setTimeout(start,3000);
+ 
+    
+}*/
+
+/*function disp()
+{  requestAnimationFrame(disp);
+    if(msg==1&&game==0) 
+    {
+    c.font = "30px Arial";
+    c.strokeText("MISS",300,300); 
+    msg=0;}
+}*/
+ animate();
+  draw();
+write();
