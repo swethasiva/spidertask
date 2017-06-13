5e32074cf33e7a61e1a0a74cae0a8f56dff22f67
swetha siva <swethadav@gmail.com>
2017-06-13 18:09:17 +0530

    Create gametry2.js
    added obstacles : speeding and slowing obstacles

--- gametry2.js
+++ gametry2.js
@@ -10,6 +10,7 @@ var score =0 ;;
 var cl=5;
 var game=1;
 var msg=0;
+var obs=0;
 
           
            var x1=100;
@@ -53,19 +54,18 @@ var msg=0;
 
 function draw ()
 {
-   //c.clearRect(0,0,innerWidth,innerHeight);
     
-c.beginPath();
-c.arc(x1,y1,50,1.5*Math.PI,0.5*Math.PI);
-c.lineWidth=5;
-c.strokeStyle="yellow";
-c.stroke();
+    c.beginPath();
+    c.arc(x1,y1,50,1.5*Math.PI,0.5*Math.PI);
+    c.lineWidth=5;
+    c.strokeStyle="yellow";
+    c.stroke();
     
-c.beginPath();
-c.moveTo(x1,y1-50);
-c.lineTo(x1,y1+50);
-c.lineWidth=2;
-c.stroke();  
+    c.beginPath();
+    c.moveTo(x1,y1-50);
+    c.lineTo(x1,y1+50);
+    c.lineWidth=2;
+    c.stroke();  
 
     c.beginPath();
     c.moveTo(x1,y1);
@@ -85,20 +85,41 @@ c.stroke();
     c.lineWidth=2;
     c.stroke();
     
+    c.fillStyle="white" ;
+    c.fillRect(650,170,25,80);
+
+    c.fillStyle="orange" ;
+    c.fillRect(750,370,25,80);
+    
+    
+    
+    
     
     c.font = "30px Arial";
-c.strokeText("SCORE:",10,50);
-c.strokeText(score,150,50);
+    c.strokeText("SCORE:",10,50);
+    c.strokeText(score,150,50);
 if(cl>=0)
     {
-c.font = "30px Arial";
-c.strokeText("ARROWS LEFT :",10,80);
-c.strokeText(cl,300,80); }
+        c.font = "30px Arial";
+        c.strokeText("ARROWS LEFT :",10,80);
+        c.strokeText(cl,300,80);
+        c.font = "20px Arial";
+        c.fillStyle="blue" ;
+        c.strokeText("OBSTACLES :",340,65);
+        c.fillStyle="orange" ;
+        c.fillRect(475,30,25,25);
+        
+        c.font = "20px Arial";
+        c.strokeText(" - SLOWS DOWN THE ARROW" , 500,50);
+         c.fillStyle="white" ;
+        c.fillRect(475,60,25,25);
+        c.font = "20px Arial";
+        c.strokeText(" - SPEEDS UP THE ARROW" , 500,80);}
     else 
         {
-c.font = "30px Arial";
-c.strokeText("GAME OVER !!! RESET TO PLAY AGAIN",300,300);
- }
+            c.font = "30px Arial";
+            c.strokeText("GAME OVER !!! RESET TO PLAY AGAIN",300,300);
+        }
    
 }
 
@@ -112,7 +133,7 @@ var dy = 1;
 function animate()
 {
     requestAnimationFrame(animate);
-     c.clearRect(0,0,innerWidth,innerHeight);
+    c.clearRect(0,0,innerWidth,innerHeight);
     draw();
   
     
@@ -188,9 +209,20 @@ function shoot()
      c.strokeStyle="red";
     c.stroke();
      
+     obstacle();
     
+     if(obs==0)
+         {
+           x2+=5; 
+         }
+     else if (obs == 1)
+         
+         {x2+=15}
      
-    x2+=5; 
+      else if (obs ==2)
+          {
+              x2+=1;
+          }
      
      
      
@@ -204,6 +236,7 @@ function shoot()
     {  score += 25;}
  
      x2=100;
+     obs=0;
      move=1;
  
 }
@@ -228,14 +261,21 @@ function start()
     game=1;
 }
 
+function obstacle()
+{
+    if(y1>170 && y1< 230 && (x2+95)== 650)
+        {
+            obs=1;
+        }
+    
+    else if(y1>370 && y1< 450 && (x2+95)== 750)
+        {
+            obs=2;
+        }
+    
+}
 
 
-/*window.onload = function() {
-    var c = document.getElementById("Canvas");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("bahubali");
-    ctx.drawImage(img, 300, 00);
-}*/
  animate();
   draw();
 write();
